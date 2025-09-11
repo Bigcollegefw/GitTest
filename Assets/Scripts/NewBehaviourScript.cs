b4e05a1610a0daa0133cf516ab746cea971ee3a3
DESKTOP-VGB9JAC\Og <reveka_liu@ogcloud.com>
2025-09-11 10:03:28 +0800

    Liuxiang提交了一个更改

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -8,6 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, World!");
+        Debug.Log("Liuxiang提交了一个更改");
     }
 
     // Update is called once per frame
